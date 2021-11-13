2f243e4494d4d889cf99506b389e3a6e3b217796
Henrik M. Arnesen <69542307+Kaosprofet@users.noreply.github.com>
2021-11-13 21:06:03 +0100

    main structure complete

--- main.c
+++ main.c
@@ -4,4 +4,26 @@ Pickguard MIDI-controller by Kaosprofet
 
 #include "includes.h"
 
+void init(void);
+void control(void);
+void normalMode(void);
+void holdMode(void);
 
+void main(void) {
+    init();
+    while(1) {
+        switch(mode) {
+            case(0):
+                normalMode();
+                break;
+            case(1):
+                holdMode();
+                break;
+        }
+        control();
+    }
+}
+
+void init(void) {
+    initUart();
+}
