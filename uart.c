2885b4f24ba57f466e64d90bb6555a831e9cc49a
Henrik M. Arnesen <69542307+Kaosprofet@users.noreply.github.com>
2021-11-13 21:57:45 +0100

    basic uart complete

---
+++ uart.c
@@ -0,0 +1,18 @@
+#include "includes.h"
+
+#define BAUD_PRESCALE ((F_CPU / (BAUD * 16UL)) - 1)
+
+void initUart(void) {
+    UBRR0H = (unsigned char)(BAUD_PRESCALE >> 8);
+	UBRR0L = (unsigned char)BAUD_PRESCALE;
+
+    UCSR0B |= (1 << TXEN0) | (1 << RXEN0);  // UART enabled
+    UCSR0C = (1 << URSEL0) | (1 << USBS0) | (3 << UCSZ00);    // 2 stop bits, 8 bit data recieve
+
+    fdevopen(transmitByte,receiveByte); //enables printf function over uart, remove after testing
+}
+
+void transmitByte(unsigned char byte) {
+    waitUntilBitIsSet(UCSR0A, UDRE0);
+    UDR0 = byte;
+}
--- uart.h
+++ uart.h
@@ -1,7 +1,6 @@
 #ifnded UART_H
 #define UART_H
 
-
 void initUart(void);
 void transmitByte(uint8_t byte);
 
