e9e5e746360dacd3472e2524926b54f6bbb064a8
Henrik M. Arnesen <69542307+Kaosprofet@users.noreply.github.com>
2021-11-13 21:09:43 +0100

    All MIDI functions added

---
+++ midi.c
@@ -0,0 +1,18 @@
+#include <avr/io.h>
+
+#include "midi.h"
+#include "uart.h"
+
+void noteOn(uint8_t note) {
+    midiSend(0x90, note, noteVelocity);
+}
+
+void noteOff(uint8_t note) {
+    midiSend(0x80, note, noteVelocity);
+}
+
+void midiSend(uint8_t byte1, uint8_t byte2, uint8_t byte3) {
+    transmitByte(byte1);
+    transmitByte(byte2);
+    transmitByte(byte3);
+}
--- midi.h
+++ midi.h
@@ -1,10 +1,8 @@
 #ifnded MIDI_H
 #define MIDI_H
 
-#include "uart.h"
-
-void midiSend(uint8_t byte1, uint8_t byte2, uint8_t byte3);
 void noteOn(uint8_t note);
 void noteOff(uint8_t note);
+void midiSend(uint8_t byte1, uint8_t byte2, uint8_t byte3);
 
 #endif
