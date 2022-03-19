52fb11c4de74cd6e582c016a15939a22158b7da0
Henrik M. Arnesen <henrik.moe.arnesen@gmail.com>
2022-03-19 01:33:20 +0100

    Pin numbering fixed. Wrong type for storing ports.

--- includes.h
+++ includes.h
@@ -26,20 +26,20 @@ typedef struct key_t {
 
 // pins needs to be fixed for the board, ad 
 key_t keys[] = {                                
-  { PD1, PORTD, PIND, 11, 0, 0 },     // B
-  { PB0, PORTB, PINB, 12, 0, 0 },     // C
-  { PD6, PORTD, PIND, 13, 0, 0 },     // C#
-  { PB1, PORTB, PINB, 14, 0, 0 },     // D
-  { PD5, PORTD, PIND, 15, 0, 0 },     // D#
-  { PB2, PORTB, PINB, 16, 0, 0 },     // E
-  { PC0, PORTC, PINC, 17, 0, 0 },     // F
-  { PD2, PORTD, PIND, 18, 0, 0 },     // F#
-  { PC1, PORTC, PINC, 19, 0, 0 },     // G
-  { PC5, PORTC, PINC, 20, 0, 0 },     // G#
-  { PC2, PORTC, PINC, 21, 0, 0 },     // A
-  { PC4, PORTC, PINC, 22, 0, 0 },     // A#
-  { PC3, PORTC, PINC, 23, 0, 0 },     // B
-  {  0, 0,  0, 0, 0 }      // end of list marker
+  { PIND1, PORTD, PIND, 11, 0, 0 },     // B
+  { PINB0, PORTB, PINB, 12, 0, 0 },     // C
+  { PIND6, PORTD, PIND, 13, 0, 0 },     // C#
+  { PINB1, PORTB, PINB, 14, 0, 0 },     // D
+  { PIND5, PORTD, PIND, 15, 0, 0 },     // D#
+  { PINB2, PORTB, PINB, 16, 0, 0 },     // E
+  { PINC0, PORTC, PINC, 17, 0, 0 },     // F
+  { PIND2, PORTD, PIND, 18, 0, 0 },     // F#
+  { PINC1, PORTC, PINC, 19, 0, 0 },     // G
+  { PINC5, PORTC, PINC, 20, 0, 0 },     // G#
+  { PINC2, PORTC, PINC, 21, 0, 0 },     // A
+  { PINC4, PORTC, PINC, 22, 0, 0 },     // A#
+  { PINC3, PORTC, PINC, 23, 0, 0 },     // B
+  {  0, 0,  0, 0, 0 }					// end of list marker
 };
 
 // Defining and initialising control button 
@@ -52,8 +52,8 @@ typedef struct controlButton_t {
 } controlButton_t;
 
 controlButton_t controlButtons[] = {
-  { PD3, PORTD, PIND,  1, 0 },  // Octave up
-  { PD4, PORTD, PIND, -1, 0 },  // Octave down
+  { PIND3, PORTD, PIND,  1, 0 },  // Octave up
+  { PIND4, PORTD, PIND, -1, 0 },  // Octave down
   // Not implemented in current design
   //{ 3, 0,  0, 0 },  // Mode switch
   //{ 4, 0,  0, 0 },  // Velocity up
