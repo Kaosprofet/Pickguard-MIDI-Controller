7e65b701f787bb24762f86adf203616fc2556e69
Henrik M. Arnesen <henrik.moe.arnesen@gmail.com>
2022-03-19 11:21:53 +0100

    Small progress, makros not working

--- uart.c
+++ uart.c
@@ -7,7 +7,7 @@ void initUart(void) {
 	UBRR0L = (unsigned char)BAUD_PRESCALE;
 
     UCSR0B |= (1 << TXEN0);  // UART enabled only TX | (1 << RXEN0)
-    UCSR0C = (1 << URSEL0) | (1 << USBS0) | (3 << UCSZ00);    // 2 stop bits, 8 bit data recieve
+    UCSR0C = (1 << USBS0) | (3 << UCSZ00);    // 2 stop bits, 8 bit data recieve // (1 << URSEL0) kanskje unødvendig?
 
     //fdevopen(transmitByte,receiveByte); //enables printf function over uart, remove after testing
 }
