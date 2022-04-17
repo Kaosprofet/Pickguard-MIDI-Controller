5a78d18e0ffc68d4ad34569d6682ccb9d065e4a6
Henrik M. Arnesen <henrik.moe.arnesen@gmail.com>
2022-04-17 10:16:19 +0200

    Removed RX and serial print refrences

--- uart.c
+++ uart.c
@@ -10,10 +10,8 @@ void initUart(void) {
     UBRR0H = (unsigned char)(BAUD_PRESCALE >> 8);
 	UBRR0L = (unsigned char)BAUD_PRESCALE;
 
-    UCSR0B |= (1 << TXEN0);  // UART enabled only TX | (1 << RXEN0)
-    UCSR0C = (1 << USBS0) | (3 << UCSZ00);    // 2 stop bits, 8 bit data recieve // (1 << URSEL0) kanskje unødvendig?
-
-    //fdevopen(transmitByte,receiveByte); //enables printf function over uart, remove after testing
+    UCSR0B |= (1 << TXEN0);  // UART enabled with only TX
+    UCSR0C = (1 << USBS0) | (3 << UCSZ00);    // 2 stop bits, 8 bit data recieve // (1 << URSEL0) kanskje unï¿½dvendig?
 }
 
 void transmitByte(unsigned char byte) {
